fd5ee87ad60b89fd5a8980fca117718c11e4d74f
satyam-shukla <satyam20@navgurukul.org>
2022-05-12 15:27:44 +0530

    first commit

---
+++ js/Login.js
@@ -0,0 +1,45 @@
+//email validate
+const isEmail = (emailVal) => {
+    var atSymbol = emailVal.indexOf("@");
+    if (atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if (dot <= atSymbol + 2) return false;
+    if (dot === emailVal.length - 1) return false;
+    return true;
+}
+
+function userLogin() {
+
+    let loginUserName, loginUserPass, loginUserEmail;
+    loginUserName = document.getElementById("loginUsername").value;
+    loginUserPass = document.getElementById("loginPassword").value;
+
+
+    let user_records = new Array();
+    user_records = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
+    if (user_records.some((v) => {
+        return v.username == loginUserName && v.password == loginUserPass;
+    })) {
+        swal(`Good Job`, "You Have Login Successfully", "success");
+        window.location.href = "https://satyam-shukla.github.io/my_blog/"
+    }
+    else if (user_records.some((v) => {
+        return v.email == loginUserName && v.password == loginUserPass;
+    })) {
+        swal(`Good Job`, "You Have Login Successfully", "success");
+        window.location.href = "https://satyam-shukla.github.io/my_blog/"
+    }
+    else if (user_records.some((v) => {
+        return v.email == loginUserName && v.password != loginUserPass;
+    })) {
+        alert("Password is Incorrect ");
+    }
+    else if (user_records.some((v) => {
+        return v.email != loginUserName;
+    })) {
+        alert("Email is not Registered ");
+    }
+    else {
+        alert('Account is not exist,Register First');
+    }
+}
---
+++ js/Register.js
@@ -0,0 +1,191 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const dateofbirth = document.getElementById('dateofbirth');
+const lastname = document.getElementById('lastname');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+//add event
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (validate()) {
+        saveData();
+        document.getElementById('form').value = ""
+        document.getElementById('firstname').value = ""
+        document.getElementById('dateofbirth').value = ""
+        document.getElementById('lastname').value = ""
+        document.getElementById('username').value = ""
+        document.getElementById('email').value = ""
+        document.getElementById('password').value = ""
+        document.getElementById('cpassword').value = ""
+
+    }
+})
+
+
+
+
+
+//email validate
+const isEmail = (emailVal) => {
+    var atSymbol = emailVal.indexOf("@");
+    if (atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if (dot <= atSymbol + 2) return false;
+    if (dot === emailVal.length - 1) return false;
+    return true;
+}
+
+
+//Strong password validate
+
+const strongPassword = (passwordVal) => {
+    if (passwordVal.match(/[a-z]/g) && passwordVal.match(
+        /[A-Z]/g) && passwordVal.match(
+            /[0-9]/g) && passwordVal.match(
+                /[^a-zA-Z\d]/g) && passwordVal.length >= 8)
+        return true;
+    else
+        return false
+}
+
+//const isUSer = (usernameVal) => {
+//var num = usernameVal;
+// if (num == Number) return false;
+
+//}
+
+const saveData = () => {
+
+    let user_records = new Array();
+    user_records = JSON.parse(localStorage.getItem("users")) ?
+        JSON.parse(localStorage.getItem("users")) : [];
+
+    if (user_records.some((e) => {
+        return e.username == username.value
+    })) {
+        alert("Sorry username alredy exists")
+    }
+    else if (user_records.some((e) => { return e.email == email.value })) {
+        console.log("shukla")
+        alert("Sorry email is already exist")
+    }
+    else {
+        user_records.push({
+            "firstname": firstname.value,
+            "lastname": lastname.value,
+            "username": username.value,
+            "dateofbirth": dateofbirth.value,
+            "email": email.value,
+            "password": password.value,
+        })
+        localStorage.setItem("users", JSON.stringify(user_records));
+        swal("Good job!", "You are Registered Successfully .. Go for Login", "success");
+    }
+}
+
+
+const validate = () => {
+    const firstnameVal = firstname.value.trim();
+    const lastnameVal = lastname.value.trim();
+    const usernameVal = username.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+    const dateofbirthVal = dateofbirth.value.trim();
+    //firstname validate
+    if (firstnameVal === "") {
+        setErrorMsg(firstname, "firstname cannot be blank");
+        return false;
+    } else if (firstnameVal.length <= 2) {
+        setErrorMsg(firstname, "firstname min 3 char");
+        return false;
+    }
+    else {
+        setSuccessMsg(firstname);
+    }
+
+    //lastName validate
+    if (lastnameVal === "") {
+        setErrorMsg(lastname, "Lastname cannot be blank");
+        return false;
+
+    } else if (lastnameVal.length <= 2) {
+        setErrorMsg(lastname, "Lastname min 3 char");
+        return false;
+
+    }
+    else {
+        setSuccessMsg(lastname);
+    }
+    // validate username
+    if (usernameVal === "") {
+        setErrorMsg(username, "username cannot be blank");
+        return false;
+    } else if (usernameVal.length <= 2) {
+        setErrorMsg(username, "username min 3 char");
+        return false;
+    }
+    else {
+        setSuccessMsg(username);
+    }
+    // validate DateofBirth
+    if (dateofbirthVal === "") {
+
+        setErrorMsg(dateofbirth, "Date of Birth cannot be blank");
+        return false;
+    }
+    else {
+        setSuccessMsg(dateofbirth);
+    }
+
+    // validate Emaiemail
+    if (emailVal === "") {
+        setErrorMsg(email, "Email cannot be blank");
+        return false;
+    } else if (!isEmail(emailVal)) {
+        setErrorMsg(email, "Not a valid Email");
+        return false;
+    } else {
+        setSuccessMsg(email);
+    }
+
+    // validate password
+    if (passwordVal === "") {
+        setErrorMsg(password, "password cannot be blank");
+        return false;
+    } else if (!strongPassword(passwordVal)) {
+        setErrorMsg(password, "Password is not strong");
+        return false;
+    }
+    else {
+        setSuccessMsg(password);
+    }
+
+    // validate repassword
+    if (cpasswordVal === "") {
+        setErrorMsg(cpassword, "password cannot be blank");
+        return false;
+    } else if (passwordVal !== cpasswordVal) {
+        setErrorMsg(cpassword, "Password is not matched");
+        return false
+    } else {
+        setSuccessMsg(cpassword);
+    }
+    return true;
+}
+
+function setErrorMsg(input, errormsgs) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = "form-controls error"
+    small.innerText = errormsgs;
+
+}
+
+function setSuccessMsg(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-controls success"
+}
